1ef150404c7c7dfb4657e627dc53a42e3a70f798
정이삭 <isaac56@naver.com>
2021-03-30 13:58:19 +0900

    feat: Implements creating User object using queryString
    쿼리스트링을 읽어서 User 객체 생성하기 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,14 +1,15 @@
 package webserver;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
-import static util.HttpRequestUtils.basicFilePath;
-import static util.HttpRequestUtils.getUrl;
+import static util.HttpRequestUtils.*;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -32,6 +33,18 @@ public class RequestHandler extends Thread {
             }
             log.debug("header: {}", line);
             String url = getUrl(line);
+
+            if (url.startsWith("/user/create")) {
+                int queryStringStartIndex = url.indexOf('?');
+                String requestPath = url.substring(0, queryStringStartIndex);
+                String queryString = url.substring(queryStringStartIndex + 1);
+
+                Map<String, String> params = parseQueryString(queryString);
+
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("created: {}", user);
+                url = "/index.html";
+            }
 
             byte[] body = Files.readAllBytes(new File(basicFilePath + url).toPath());
 
